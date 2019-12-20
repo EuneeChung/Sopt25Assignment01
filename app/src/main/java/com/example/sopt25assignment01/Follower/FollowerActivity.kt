65f775d0a0e2991ad2415542197e5772a69b3aaa
EuneeChung <chungee375@gmail.com>
2019-12-20 21:33:11 +0900

    Recycler VIew

--- app/src/main/java/com/example/sopt25assignment01/Follower/FollowerActivity.kt
+++ app/src/main/java/com/example/sopt25assignment01/Follower/FollowerActivity.kt
@@ -2,16 +2,42 @@ package com.example.sopt25assignment01.Follower
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.sopt25assignment01.Data.Followers
 import com.example.sopt25assignment01.MyProfileFragment
 import com.example.sopt25assignment01.R
 
 class FollowerActivity : AppCompatActivity() {
 
+    private lateinit var rvFollower: RecyclerView
+    private lateinit var followerViewAdapter : FollowerViewAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_follower)
 
-        val myProfileFragment= MyProfileFragment()
+        rvFollower=findViewById(R.id.rvFollower)
+        followerViewAdapter = FollowerViewAdapter(this)
+
+        rvFollower.adapter = followerViewAdapter
+        rvFollower.layoutManager =LinearLayoutManager(this)
+        followerViewAdapter.data = listOf(
+            Followers(
+                name="신승민",
+                userName="greedy0110"
+            ),
+            Followers(
+                name="신승민",
+                userName="greedy0110"
+            ),
+            Followers(
+                name="신승민",
+                userName="greedy0110"
+            )
+
+
+        )
+        followerViewAdapter.notifyDataSetChanged()
 
 
     }
--- app/src/main/java/com/example/sopt25assignment01/MainActivity.kt
+++ app/src/main/java/com/example/sopt25assignment01/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.sopt25assignment01
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sopt25assignment01.DB.SharedPreferenceController
+import com.example.sopt25assignment01.Follower.FollowerActivity
 import com.example.sopt25assignment01.SignIn_Up.SignInActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class MainActivity : AppCompatActivity() {
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         val fragment1=DummyFragment()
         fragment1.name="frag1"
@@ -19,11 +25,18 @@ class MainActivity : AppCompatActivity() {
         transaction.add(R.id.container_some,fragment1)
 
         transaction.commit()
-
+        val userInfo=SharedPreferenceController.getSignUpINfo(this)
+        if(userInfo[0]!="") login.text= "follower"
 
         login.setOnClickListener {
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
+            if(userInfo[0]!=""){
+                val intent = Intent(this, FollowerActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                val intent = Intent(this, SignInActivity::class.java)
+                startActivity(intent)
+            }
         }
         goToMyProfile.setOnClickListener {
             val intent2 = Intent(this,MyPageActivity::class.java)
